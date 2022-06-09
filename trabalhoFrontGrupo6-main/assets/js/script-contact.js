c1356f00ac0369fedea68e59e5173d6ca5ab675f
gbassan09 <102334775+gbassan09@users.noreply.github.com>
2022-06-09 15:52:02 -0300

    Add files via upload

---
+++ trabalhoFrontGrupo6-main/assets/js/script-contact.js
@@ -0,0 +1,42 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 0) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", validar);
+
+function validar(e) {
+
+  const firstNameField = document.getElementById("firstname");
+  let valid = true;
+
+  if (!firstNameField.value) {
+    const nameError = document.getElementById("nameError");
+    nameError.classList.add("visible");
+    firstNameField.classList.add("invalid");
+    nameError.setAttribute("aria-hidden", false);
+    nameError.setAttribute("aria-invalid", true);
+  }
+  return valid;
+}
